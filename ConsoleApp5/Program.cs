f1cea8f4eb5cf7320aebb6ab67514f27ef30312e
aseallotfe <133778455+aseallotfe@users.noreply.github.com>
2023-05-21 22:53:05 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp5;
+using System.ComponentModel.DataAnnotations;
+Console.WriteLine("enter first name");
+string x = Console.ReadLine();
+Console.WriteLine("enter last name");
+string y = Console.ReadLine();
+Console.WriteLine("enter your email");
+string v = Console.ReadLine();
+person re = new person();
+re.GetFullName(x, y);
+re.Print(x, y, v);
+
---
+++ ConsoleApp5/person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class person
+    {
+    
+        public string firstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string lastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string email
+        {
+            get { return email; }
+            set
+            {        
+                    email = value;  
+            }
+        }
+            public void GetFullName(string firstName,string lastName)
+        {
+            Console.WriteLine($"fullname is {firstName} {lastName}");
+        }
+        public void Print(string firstName,string lastName,string email)
+        {
+            Console.WriteLine($"fullname is {firstName} {lastName} ");
+            if (email.Contains("@") == true && email.EndsWith("com") == true)
+            { Console.WriteLine($"email is {email}"); }
+            else
+            {
+
+                Console.WriteLine("Worang value");
+            }
+
+        }
+    }
+}
